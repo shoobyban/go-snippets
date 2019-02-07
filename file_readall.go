0933809dda19f0fb8bd24db82c0bf27a651ca24d
Sam Ban <sam@gpmd.co.uk>
2019-02-07 10:19:26 +0000

    file read all

---
+++ file_readall.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_readall {filename}")
+		os.Exit(0)
+	}
+	filename := os.Args[1]
+	fh, err := os.Open(filename)
+	if err != nil {
+		log.Panicf("can't open file %s", filename)
+	}
+	filebytes, err := ioutil.ReadAll(fh)
+	if err != nil {
+		log.Panicf("can't read file %s", filename)
+	}
+	fmt.Printf("File content as string: %s", string(filebytes))
+}
